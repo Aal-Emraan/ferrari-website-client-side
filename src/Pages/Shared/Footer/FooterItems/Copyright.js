0b5d090c729113a708b3651e89b2afe0700e3bcf
Aal Emraan <developer.aalemraan@gmail.com>
2022-01-07 19:07:40 +0600

    copyright part added

--- src/Pages/Shared/Footer/FooterItems/Copyright.js
+++ src/Pages/Shared/Footer/FooterItems/Copyright.js
@@ -1,13 +1,27 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Copyright = () => {
   return (
-    <div>
-      <Typography variant="h6" style={{ color: "white" }}>
-        &copy; Copyright. All rights reserved.
+    <Box
+      style={{
+        textAlign: "center",
+        color: "white",
+        borderTop: "2px solid gray",
+        padding: "20px 0",
+      }}
+    >
+      <Typography variant="h6">
+        Ferrari S.p.A. Copyright © 2022 - All Rights Reserved Powered by YOOX
+        NET-A-PORTER GROUP
       </Typography>
-    </div>
+      <Typography>
+        <span>Site map</span>
+        <span style={{ marginLeft: "20px" }}>Cookie Policy</span>
+        <span style={{ marginLeft: "20px" }}>Privacy Policy</span>
+        <span style={{ marginLeft: "20px" }}> Vendor information</span>
+      </Typography>
+    </Box>
   );
 };
 
